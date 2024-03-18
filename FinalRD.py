07de8b0f78798004a3f08dc97659dc25e4d16138
IsaacHong2006 <162154310+IsaacHong2006@users.noreply.github.com>
2024-03-18 10:43:57 -0400

    Update FinalRD.py

--- FinalRD.py
+++ FinalRD.py
@@ -1,21 +1,21 @@
-import pygame
-from sys import exit
+# import pygame
+# from sys import exit
 
-pygame.init()
-screen = pygame.display.set_mode((1800, 800))
-pygame.display.set_caption("Inevitability")
-clock = pygame.time.Clock()
+# pygame.init()
+# screen = pygame.display.set_mode((1800, 800))
+# pygame.display.set_caption("Inevitability")
+# clock = pygame.time.Clock()
 
-surface_Test = pygame.Surface((100,200))
-surface_Test.fill('purple')
-#Game is going to be running in a for loop until it is exited
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-    screen.blit(surface_Test, (750,300)) #backgroundcolour
-    #draw all the elements
-    #update everything
-    pygame.display.update()
-    clock.tick(60)
+# surface_Test = pygame.Surface((100,200))
+# surface_Test.fill('purple')
+# #Game is going to be running in a for loop until it is exited
+# while True:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#             exit()
+#     screen.blit(surface_Test, (750,300)) #backgroundcolour
+#     #draw all the elements
+#     #update everything
+#     pygame.display.update()
+#     clock.tick(60)
